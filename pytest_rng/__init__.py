acb86d46fc5f40e59c4ded29e7d6ec06a41847dd
Eric Hunsberger <eric.hunsberger@appliedbrainresearch.com>
2021-01-04 11:58:12 -0500

    Update copyright dates to 2021

--- pytest_rng/__init__.py
+++ pytest_rng/__init__.py
@@ -2,5 +2,5 @@
 
 from .version import version as __version__
 
-__copyright__ = "2019-2019 pytest_rng contributors"
+__copyright__ = "2019-2021 pytest_rng contributors"
 __license__ = "MIT license"
